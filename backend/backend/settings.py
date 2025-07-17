2d0151e003813917dc25e3fa25c72ed95e9c2d0d
Vinayak Tiller <vinayaktiller@gmail.com>
2025-07-18 00:32:11 +0530

    reducing the overhead by removing the sstyling from the backend api page

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,7 +118,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 SIMPLE_JWT = {
